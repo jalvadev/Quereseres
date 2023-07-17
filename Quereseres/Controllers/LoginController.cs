eab46b2fd654587c4f78f971c24dff09c467ef37
Jose Álvarez <josealvarezvivar@gmail.com>
2023-07-17 17:57:04 +0200

    Getting user from DB.

--- Quereseres/Controllers/LoginController.cs
+++ Quereseres/Controllers/LoginController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Quereseres.Helpers;
 using Quereseres.Interfaces;
 using Quereseres.Models;
 
@@ -23,7 +24,17 @@ namespace Quereseres.Controllers
         public ActionResult Login([FromForm] string email, string password)
         {
 
+            // 1 - Checking mandatory fields.
+            if (email == null || password == null)
+                return null;
+
+            // 2 - Hashs password and get user by credentials.
+            password = CryptoHelper.GenerateSHA512String(password);
             User fullUser = _userRepository.GetUserByCredentials(email, password);
+
+            // 3 - Validate user.
+            if (fullUser == null)
+                return null;
 
             return Redirect("/Home");
         }
